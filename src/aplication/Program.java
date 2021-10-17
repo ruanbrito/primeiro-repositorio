e597d645a7264f9713f1c6643ff7e44706afb2df
ruanbrito <rpablobrito@gmail.com>
2021-10-17 19:01:49 -0300

    teste e execução do comando clone e trabalhando com projeto já existente no github

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -4,7 +4,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Iniciando o estudo em git.");
+		System.out.println("1 - Iniciando o estudo em git.");
+		System.out.println("2 - comando de clone do repositorio\nexecutado com sucesso");
 	}
 
 }
